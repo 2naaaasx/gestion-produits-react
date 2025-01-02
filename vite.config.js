e424e03256f4db692dc4dcc21db8fd5b82749384
Anas Oubrahim <anasioubrahim777@gmail.com>
2025-01-03 00:01:54 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/gestion-produits-react",
+  host: '0.0.0.0',
+  port: process.env.PORT || 3000,
 })
